9af5ff05529e3729144d56997a1994157f37cdcf
TaliBerzins <tali.berzins@outlook.com>
2026-04-02 13:25:48 -0400

    Created all numeric tests

--- tests/test_rotation_utils.py
+++ tests/test_rotation_utils.py
@@ -6,8 +6,16 @@ To test the rotation_utils module
 import pytest
 from rotation_utils import adjust_rotation
 
-def test_rotation_100():
-    value = adjust_rotation(100)
-    assert value == 100
+def test_rotation_numeric_inputs():
+     assert adjust_rotation(100) == 100
+     assert adjust_rotation(460) == 100
+     assert adjust_rotation(820) == 100
+     assert adjust_rotation(-100) == 260
+     assert adjust_rotation(-460) == 260
+     assert adjust_rotation(-820) == 260
+
+
+def test_non_numeric():
+     assert adjust_rotation("string") == 
 
 
